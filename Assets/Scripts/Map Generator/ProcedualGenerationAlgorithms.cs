72955643588bd365f58084f467fdc452685291bc
skerbos <79687493+skerbos@users.noreply.github.com>
2023-11-07 13:06:40 +0800

    feat: Adding biome values array to GeneratedMapData

--- Assets/Scripts/Map Generator/ProcedualGenerationAlgorithms.cs
+++ Assets/Scripts/Map Generator/ProcedualGenerationAlgorithms.cs
@@ -116,3 +116,15 @@ public static class Direction2D
         return cardinalDirectionsList[Random.Range(0, cardinalDirectionsList.Count)];
     }
 }
+
+public enum Biomes
+{
+    None,
+    RedSpawn,
+    BlueSpawn,
+    One,
+    Two,
+    Three,
+    Four,
+    Five
+}
--- Assets/Scripts/Network Scripts/Structs/GeneratedMapData.cs
+++ Assets/Scripts/Network Scripts/Structs/GeneratedMapData.cs
@@ -7,10 +7,12 @@ public struct GeneratedMapData : INetworkSerializable
 {
     public int Size;
     public Vector2Int[] FloorPositions;
+    public Biomes[] BiomeTypes;
 
     public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
     {
         serializer.SerializeValue(ref Size);
         serializer.SerializeValue(ref FloorPositions);
+        serializer.SerializeValue(ref BiomeTypes);
     }
 }
