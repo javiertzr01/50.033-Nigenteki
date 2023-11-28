a943bb265cf7c4c31bfeeca19d9de2a0ffdb7502
skerbos <christopher_wah@mymail.sutd.edu.sg>
2023-11-28 18:54:00 +0800

    fix: WIP basic projectile, adding back charge ultimate
    testing if destroy server rpc is still called

--- Assets/Scripts/Combat Scripts/Projectile Scripts/BasicProjectile.cs
+++ Assets/Scripts/Combat Scripts/Projectile Scripts/BasicProjectile.cs
@@ -15,7 +15,7 @@ public class BasicProjectile : Projectile
             if (other.transform.GetComponent<PlayerController>().teamId.Value != teamId.Value)
             {
                 other.transform.GetComponent<PlayerController>().TakeDamageServerRpc(Damage, other.transform.GetComponent<NetworkObject>().OwnerClientId);
-                //instantiatingArm.ChargeUltimate(Damage, 100);
+                instantiatingArm.ChargeUltimate(Damage, 100);
 
                 DestroyServerRpc();
             }
