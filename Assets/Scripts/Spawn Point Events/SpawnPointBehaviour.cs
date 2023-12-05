fa8b4b262e9b344c4a88c11bf1f34d21b8124621
gnuppie <pung.junhao.louis@gmail.com>
2023-12-05 16:11:01 +0800

    chore: Added force field for spawnpoint

--- Assets/Scripts/Spawn Point Events/SpawnPointBehaviour.cs
+++ Assets/Scripts/Spawn Point Events/SpawnPointBehaviour.cs
@@ -8,9 +8,54 @@ public class SpawnPointBehaviour : MonoBehaviour
     [SerializeField]
     private float spawnHealingValue = 50f;
 
+    private SpriteRenderer spriteRenderer;
+    [SerializeField] private float visibilityDistance = 10f; // Distance within which the SilkWeb becomes visible to the enemy team
+    [SerializeField] private float maxOpacityDistance = 15f; // Distance at which the SilkWeb is fully transparent to the enemy team
+
+    private void Update()
+    {
+        if (NetworkManager.Singleton.LocalClient.PlayerObject.TryGetComponent(out PlayerController player))
+        {
+            UpdateVisibility(player);
+        }
+    }
+
+    private void UpdateVisibility(PlayerController player)
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distanceToPlayer <= visibilityDistance)
+        {
+            spriteRenderer.enabled = true;
+            float opacity = 0.4f - (distanceToPlayer / maxOpacityDistance);
+            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, opacity);
+        }
+        else
+        {
+            spriteRenderer.enabled = false;
+        }
+    }
+
+
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.enabled = false;
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        SpawnProtection(other);
+        if (other.CompareTag("Player"))
+        {
+            UpdateVisibility(other.GetComponent<PlayerController>());
+        }
+        else
+        {
+            SpawnProtection(other);
+
+        }
+
     }
 
     private void OnTriggerStay2D(Collider2D other)
@@ -37,6 +82,14 @@ public class SpawnPointBehaviour : MonoBehaviour
         }
 
         SpawnProtection(other);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            UpdateVisibility(other.GetComponent<PlayerController>());
+        }
     }
 
 
